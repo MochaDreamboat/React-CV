03b01f252828c442515045a0b9206a8bacb58fc2
mochadreamboat <jersotmac@gmail.com>
2022-08-30 22:37:41 -0500

    Implemented Header, ContactInfo, and Experience components in App render. Pending user ability to change information.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import Header from "./components/Header.js";
 import Experience from "./components/Experience.js";
-
+import ContactInfo from "./components/ContactInfo.js";
 const pastJobs = 
 [
   {
@@ -15,12 +16,31 @@ class App extends Component {
     super();
 
     this.state = {
-    
+     personalInfo: {
+      name: 'John Smith',
+      title: 'Web Developer',
+      email: 'johnsmith@gmail.com',
+      phone: '111-111-1111',
+      location: 'Chicago, IL',
+      website: 'mywebsite.com',
+      summary: 'A fledgling developer looking to make moooves.',
+      skills: [],
+      pic: undefined
+     },
+
+     pastJobs: []
     }
   }
 
   render() {
-    return <Experience pastJobs={pastJobs} />
+    const {name, title, summary, email, phone, location, website } = this.state.personalInfo;
+    return (
+    <div>
+      <Header name={name} title={title} summary={summary}/>
+      <ContactInfo email={email} phone={phone} location={location} website={website} /> 
+      <Experience pastJobs={pastJobs} />
+    </div>
+    )
   }
 }
 
