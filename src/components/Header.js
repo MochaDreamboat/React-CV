c5a422bc52299a390f08278edd3c8225ed370034
mochadreamboat <jersotmac@gmail.com>
2022-08-31 22:21:17 -0500

    Added Form component to take user input. Can toggle view on and off using toggle function.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,53 @@ import React, {Component} from "react";
 class Header extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            formVisible: false,
+            changes: {
+                changedName: '',
+                changedTitle: '',
+                changedSummary: ''
+            }
+        }
+
+        this.toggle = this.toggle.bind(this);
     }
 
+    toggle () {
+        const visible = (this.state.formVisible ? false : true);
+        this.setState({formVisible: visible})
+    }
+
+
     render() {
-        const { name, title, summary } = this.props;
+        const { name, title, summary, changeInfo } = this.props;
     
         return (
             <div>
-                <h1>{name}</h1>
-                <h2>{title}</h2>
-                <p>{summary}</p>
+                <section>
+                    <h1>{name}</h1>
+                    <h2>{title}</h2>
+                    <p>{summary}</p>
+                    <button onClick={this.toggle}>Edit</button>
+                    {this.state.formVisible && <Form />}
+                </section>
             </div>
  );
     }
 }
 
+const Form = (props) => (
+    <form id="edit-header">
+        <label for="name">Change Name</label>
+        <input id="name" onChange={null}/>
+        <label for="title">Change Title</label>
+        <input id="title" onChange={null}/>
+        <label for="summary">Change Summary</label>
+        <input id="summary" onChange={null}/>
+        <button onClick={props.submitFunction}>Make Changes</button>    
+    </form>
+)
+
 export default Header;
+export { Form };
