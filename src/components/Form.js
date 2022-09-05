b837150dea7346e73e266a31d741107ba7d59f41
mochadreamboat <jersotmac@gmail.com>
2022-09-04 22:53:21 -0500

    Enabled input fields to modify changes object in state for personal info. Form can now submit changes, resulting in re-rendering of new personal info values.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Form = (props) => (
-    <form id="edit-header">
-        <label for="name">Change Name</label>
-        <input id="name" onChange={null}/>
-        <label for="title">Change Title</label>
-        <input id="title" onChange={null}/>
-        <label for="summary">Change Summary</label>
-        <input id="summary" onChange={null}/>
-        <button onClick={props.submitFunction}>Make Changes</button>    
-    </form>
-)
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render () {
+        const { name, title, summary } = this.props.default;
+        return (
+            <form id="edit-header">
+                <label for="name">Change Name</label>
+                <input onChange={this.props.edit} id="name" placeholder={name} />
+                <label for="title">Change Title</label>
+                <input onChange={this.props.edit} id="title" placeholder={title}/>
+                <label for="summary">Change Summary</label>
+                <input onChange={this.props.edit} id="summary" placeholder={summary} />
+                <button onClick={this.props.submit}>Make Changes</button>    
+            </form>
+        )
+    }
+}
 
 
 export default Form;
