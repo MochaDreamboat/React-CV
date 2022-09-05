04211f3c6de9cca6b840b7fb36667488357b5077
mochadreamboat <jersotmac@gmail.com>
2022-09-04 22:51:02 -0500

    Wrote functions for editing and submitting changes to personal info. Passed functions as prop to Form component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Form from "./components/Form.js";
 import Header from "./components/Header.js";
 import Experience from "./components/Experience.js";
 import ContactInfo from "./components/ContactInfo.js";
+
 const pastJobs = 
 [
   {
@@ -20,14 +21,23 @@ class App extends Component {
      personalInfo: {
       name: 'John Smith',
       title: 'Web Developer',
+      summary: 'A fledgling developer looking to make moooves.',
       email: 'johnsmith@gmail.com',
       phone: '111-111-1111',
       location: 'Chicago, IL',
       website: 'mywebsite.com',
-      summary: 'A fledgling developer looking to make moooves.',
       skills: [],
       pic: undefined
      },
+
+     changes: {
+      personalInfo: {
+        name: '',
+        title: '',
+        summary: ''
+      }
+     },
+
      formVisible: false,
      pastJobs: []
     }
@@ -35,21 +45,48 @@ class App extends Component {
     this.toggleForm = this.toggleForm.bind(this);
   }
 
-  changePersonalInfo = (changes) => {
-    this.setState({personalInfo: {name: changes.changedName, title: changes.changedTitle, summary: changes.changedSummary}})
-  }
-
   toggleForm () {
     const visible = (this.state.formVisible ? false : true);
     this.setState({formVisible: visible})
-}
+  }
+
+  handleChange = (e) => {
+    let changedValue = e.target.id;
+    this.setState({
+      changes: {
+        personalInfo: {
+          ...this.state.changes.personalInfo,
+          [changedValue]: e.target.value
+        }
+      }
+    })
+  }
+
+  submitChanges = (e) => {
+    e.preventDefault();
+    this.setState({
+      personalInfo: {
+        ...this.state.personalInfo,
+        name: this.state.changes.personalInfo.name,
+        title: this.state.changes.personalInfo.title,
+        summary: this.state.changes.personalInfo.summary
+      },
+      changes: {
+        name: '',
+        title: '',
+        summary: ''
+      },
+      formVisible: false
+    })
+  }
+
 
   render() {
     const {name, title, summary, email, phone, location, website } = this.state.personalInfo;
     return (
     <div>
       <Header name={name} title={title} summary={summary} showForm={this.toggleForm} />
-      {this.state.formVisible && <Form />}
+      {this.state.formVisible && <Form edit={this.handleChange} default={this.state.personalInfo} submit={this.submitChanges}/>}
       <ContactInfo email={email} phone={phone} location={location} website={website} /> 
       <Experience pastJobs={pastJobs} />
     </div>
