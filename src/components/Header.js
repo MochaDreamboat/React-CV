77ef5f6835bc93d00fd222a5638ff4411559212c
mochadreamboat <jersotmac@gmail.com>
2022-09-04 22:54:20 -0500

    Cleaned up unnecessary code.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,11 @@
 // personalInfo.title
 //personalInfo.pic
 
-import React, {Component} from "react";
+import React, { Component } from "react";
 class Header extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            changes: {
-                changedName: '',
-                changedTitle: '',
-                changedSummary: ''
-            }
-        }
     }
-
-    toggle () {
-        const visible = (this.state.formVisible ? false : true);
-        this.setState({formVisible: visible})
-    }
-
 
     render() {
         const { name, title, summary, showForm } = this.props;
