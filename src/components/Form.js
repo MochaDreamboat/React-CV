87f7e9efa25259647facebc50121e725e46aeffa
mochadreamboat <jersotmac@gmail.com>
2022-09-14 22:39:02 -0500

    Changed Form from function component to class component. Added header class to input elements.

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,11 +11,11 @@ class Form extends Component {
         return (
             <form id="edit-header">
                 <label for="name">Change Name</label>
-                <input onChange={this.props.edit} id="name" placeholder={name} />
+                <input onChange={this.props.edit} class="header" id="name" placeholder={name} />
                 <label for="title">Change Title</label>
-                <input onChange={this.props.edit} id="title" placeholder={title}/>
+                <input onChange={this.props.edit} class="header" id="title" placeholder={title}/>
                 <label for="summary">Change Summary</label>
-                <input onChange={this.props.edit} id="summary" placeholder={summary} />
+                <input onChange={this.props.edit} class="header" id="summary" placeholder={summary} />
                 <button onClick={this.props.submit}>Make Changes</button>    
             </form>
         )
@@ -24,3 +24,7 @@ class Form extends Component {
 
 
 export default Form;
+
+// Find way to generalize form for each respective section!
+
+// Header, ContactInfo, Experience
