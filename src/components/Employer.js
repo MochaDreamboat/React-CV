9dec5efaf99fc7340d522de83aec765422025c69
mochadreamboat <jersotmac@gmail.com>
2022-09-21 23:31:37 -0500

    Added element for displaying dates worked.

--- src/components/Employer.js
+++ src/components/Employer.js
@@ -5,6 +5,7 @@ function Employer ({ details }) {
         <div>
             <h3>{details.position}</h3>
             <h3>{details.company}</h3>
+            <h3>{details['Dates Worked']}</h3>
             <ul>
                 {details.duties.map((duty) => {
                     return <li>{duty}</li>
