c833c57644cb56a8e85db873f859c8f2d944e086
mochadreamboat <jersotmac@gmail.com>
2022-09-26 23:38:04 -0500

    Beautified script / Wrote creation and submission functions for Education section / removed unnecessary constructor for Employer object.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import Form from "./components/Form.js";
 import Header from "./components/Header.js";
-import Experience from "./components/Experience.js";
 import ContactInfo from "./components/ContactInfo.js";
+import Experience from "./components/Experience.js";
+import Education from "./components/Education.js";
+
 import uniqid from "uniqid";
 
 class App extends Component {
@@ -10,86 +12,91 @@ class App extends Component {
     super();
 
     this.state = {
-     header: {
-      id: 'a',
-      name: 'John Smith',
-      title: 'Web Developer',
-      summary: 'A fledgling developer looking to make moooves.',
-      skills: [],
-      pic: undefined
-     },
-
-     contactInfo: {
-      id: 'b',
-      email: 'johnsmith@gmail.com',
-      phone: '111-111-1111',
-      location: 'Chicago, IL',
-      website: 'mywebsite.com',
-     },
-
-     employers: {
-      'someid1': {
-          id: '1',
-          'company': 'Legends',
-          'position': 'Bartender',
-          'Dates Worked': '09/2017 - 07/2020',
-          'duties': ['lorem ipsum','lorem ipsum','lorem ipsum']
-      },
-
-    },
-    employer: {
-      'company': '',
-      'position': '',
-      'Dates Worked': '',
-      'duties': []
-    },
-
-     changes: {
       header: {
-        name: '',
-        title: '',
-        summary: ''
+        id: "a",
+        name: "John Smith",
+        title: "Web Developer",
+        summary: "A fledgling developer looking to make moooves.",
+        skills: [],
+        pic: undefined,
       },
+
       contactInfo: {
-        email: '',
-        phone: '',
-        location: '',
-        website: ''
+        id: "b",
+        email: "johnsmith@gmail.com",
+        phone: "111-111-1111",
+        location: "Chicago, IL",
+        website: "mywebsite.com",
       },
 
-     },
+      employers: {
+        someid1: {
+          company: "Legends",
+          position: "Bartender",
+          "Dates Worked": "09/2017 - 07/2020",
+          duties: ["lorem ipsum", "lorem ipsum", "lorem ipsum"],
+        },
+      },
 
-     
+      education: {
+        institution1: {
+          graduation: "Bachelor's Degree in Behavioral Neuroscience",
+          almaMater: "University of Illinois at Urbana-Champaign",
+          attended: "2019-2021",
+        },
+      },
 
-     formVisible: {
-      header: false,
-      contactInfo: false,
-      experience: false
-     },
+      // For submitting new employer. Object pushed to employers state and cleared upon submission.
+      employer: {
+        company: "",
+        position: "",
+        "Dates Worked": "",
+        duties: [],
+      },
 
-     pastJobs: []
-    }
+      educator: {
+        graduation: "",
+        almaMater: "",
+        attended: "",
+      },
+
+      changes: {
+        header: {
+          name: "",
+          title: "",
+          summary: "",
+        },
+        contactInfo: {
+          email: "",
+          phone: "",
+          location: "",
+          website: "",
+        },
+      },
+
+      formVisible: {
+        header: false,
+        contactInfo: false,
+        experience: false,
+        education: false,
+      },
+
+      pastJobs: [],
+    };
 
     this.toggleForm = this.toggleForm.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.submitChanges = this.submitChanges.bind(this);
   }
 
-
-  Employer = (company, position, duties) => {
-    this.company = company;
-    this.position = position;
-    this.duties = duties;
-  }
-
   toggleForm = (e) => {
-    const changeToggle = (this.state.formVisible[e.target.id] ? false : true)
+    const changeToggle = this.state.formVisible[e.target.id] ? false : true;
     this.setState({
       formVisible: {
-        [e.target.id]: changeToggle
-      }
-    })
-  }
+        [e.target.id]: changeToggle,
+      },
+    });
+  };
 
   handleCreate = (e) => {
     const addTo = e.target.className;
@@ -97,13 +104,13 @@ class App extends Component {
     this.setState({
       [addTo]: {
         ...this.state[addTo],
-        [addedValue]: e.target.value
-      }
-    })
-  }
+        [addedValue]: e.target.value,
+      },
+    });
+  };
 
-  // For submitting new Education / Employer
-  newSubmission = (e) => {
+  // For submitting new Employer
+  newEmployerSubmission = (e) => {
     // Push new employer staged in state to Employers.
     e.preventDefault();
     this.setState({
@@ -111,11 +118,23 @@ class App extends Component {
         ...this.state.employers,
         [uniqid()]: {
           ...this.state.employer,
-          'duties': this.state.employer.duties.split(', ')
+          duties: this.state.employer.duties.split(", "),
+        },
+      },
+    });
+  };
+
+  newEducatorSubmission = (e) => {
+    e.preventDefault();
+    this.setState({
+      education: {
+        ...this.state.education,
+        [uniqid()]: {
+          ...this.state.educator
         }
       }
     })
-  }
+  };
 
   handleChange = (e) => {
     let changeCategory = e.target.className;
@@ -124,11 +143,11 @@ class App extends Component {
       changes: {
         [changeCategory]: {
           ...this.state.changes[changeCategory],
-          [changedValue]: e.target.value
-        }
-      }
-    })
-  }
+          [changedValue]: e.target.value,
+        },
+      },
+    });
+  };
 
   submitChanges = (e) => {
     e.preventDefault();
@@ -137,35 +156,78 @@ class App extends Component {
     this.setState({
       [changeCategory]: {
         ...this.state[changeCategory],
-        ...edits
+        ...edits,
       },
-      formVisible: false
-    })
+      formVisible: false,
+    });
     for (let field in edits) {
       this.setState({
         edits: {
           ...edits,
-          [field]: ''
-        }
-      })
+          [field]: "",
+        },
+      });
     }
-    }
-
+  };
 
   render() {
-    const {name, title, summary } = this.state.header;
+    const { name, title, summary } = this.state.header;
     const { email, phone, location, website } = this.state.contactInfo;
     const employers = this.state.employers;
     return (
-    <div>
-      <Header name={name} title={title} summary={summary} showForm={this.toggleForm} />
-      {this.state.formVisible.header && <Form willChange={"header"} fields={['name', 'title', 'summary']} edit={this.handleChange} submit={this.submitChanges}/>}
-      <ContactInfo email={email} phone={phone} location={location} website={website} showForm={this.toggleForm} />
-      {this.state.formVisible.contactInfo && <Form willChange={"contactInfo"} fields={['email', 'phone', 'location', 'website']} edit={this.handleChange} submit={this.submitChanges}/>}
-      <Experience employers = { employers } showForm={this.toggleForm}/>
-      {this.state.formVisible.experience && <Form willChange={"employer"} fields = {['company', 'position', 'Dates Worked', 'duties']} edit ={this.handleCreate} submit={this.newSubmission} />}
-    </div>
-    )
+      <div>
+        <Header
+          name={name}
+          title={title}
+          summary={summary}
+          showForm={this.toggleForm}
+        />
+        {this.state.formVisible.header && (
+          <Form
+            willChange={"header"}
+            fields={["name", "title", "summary"]}
+            edit={this.handleChange}
+            submit={this.submitChanges}
+          />
+        )}
+        <ContactInfo
+          email={email}
+          phone={phone}
+          location={location}
+          website={website}
+          showForm={this.toggleForm}
+        />
+        {this.state.formVisible.contactInfo && (
+          <Form
+            willChange={"contactInfo"}
+            fields={["email", "phone", "location", "website"]}
+            edit={this.handleChange}
+            submit={this.submitChanges}
+          />
+        )}
+        <Experience employers={employers} showForm={this.toggleForm} />
+        {this.state.formVisible.experience && (
+          <Form
+            willChange={"employer"}
+            fields={["company", "position", "Dates Worked", "duties"]}
+            edit={this.handleCreate}
+            submit={this.newEmployerSubmission}
+          />
+        )}
+        <Education
+          education={this.state.education}
+          showForm={this.toggleForm}
+        />
+        {this.state.formVisible.education && (
+          <Form
+            willChange={"educator"}
+            fields={["graduation", "almaMater", "attended"]}
+            edit={this.handleCreate}
+            submit={this.newEducatorSubmission}
+          />
+        )}
+      </div>
+    );
   }
 }
 
