36d10fdc08c4e74940319513931ff56d36f98df5
mochadreamboat <jersotmac@gmail.com>
2022-10-02 20:43:17 -0500

    Added conditional rendering for name, title, and summary sections.

--- src/components/sandbox.js
+++ src/components/sandbox.js
@@ -1,41 +1,100 @@
 // For messing around
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function Header2 () {
-    // Each field has a state object holding key/value pair for content and whether or not user is editing.
+function Header2() {
+  // Each field has a state object holding key/value pair for content and whether or not user is editing.
 
-    const [name, setName] = useState({field: "insert name here", editingOn: false});
-    const [title, setTitle] = useState({field: "insert title here", editingOn: false});
-    const [summary, setSummary] = useState({field: "lorem ipsum something something...", editingOn: false}); 
-    
-    useEffect(() => {
+  const [name, setName] = useState({
+    fieldText: "insert name here",
+    editingOn: false,
+  });
+  const [title, setTitle] = useState({
+    fieldText: "insert title here",
+    editingOn: false,
+  });
+  const [summary, setSummary] = useState({
+    fieldText: "lorem ipsum something something...",
+    editingOn: false,
+  });
 
-    }, [ ])
-    
-
-    console.log(name.editingOn);
-    // Should include a button that appears when hovered
-    // That lets user edit field in place.
-    // Conditonal rendering based on whether or not editing is T or F.
-    return (
-        <section>
-            {name.editingOn ? 
-            <div>
-                <input>{name.field}</input>
-                <button></button>
-            </div>
-            :
-            <div>
-                <h1 id="name">{name.field}</h1><button onClick={() => setName({field: name.field, editingOn: (editingOn ? false : true)})}>Edit</button>
-            </div>
+  // Should include a button that appears when hovered
+  // That lets user edit field in place.
+  // Conditonal rendering based on whether or not editing is T or F.
+  return (
+    <section>
+      {name.editingOn ? (
+        <div>
+          <input placeholder={name.fieldText} />
+          <button
+            onClick={() =>
+              setName({ fieldText: name.fieldText, editingOn: false })
             }
-            
-            <h2 id="title">{title.field}</h2>
-            <p id="summary">{summary.field}</p>
-            {/* <button id="header" onClick={showForm}>Edit</button> */}
-        </section>
-);
+          >
+            X
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1 id="name">{name.fieldText}</h1>
+          <button
+            onClick={() =>
+              setName({ fieldText: title.fieldText, editingOn: true })
+            }
+          >
+            Edit
+          </button>
+        </div>
+      )}
 
+      {title.editingOn ? (
+        <div>
+          <input placeholder={title.fieldText} />
+          <button
+            onClick={() =>
+              setTitle({ fieldText: title.fieldText, editingOn: false })
+            }
+          >
+            X
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2 id="title">{title.fieldText}</h2>
+          <button
+            onClick={() =>
+              setTitle({ fieldText: title.fieldText, editingOn: true })
+            }
+          >
+            Edit
+          </button>
+        </div>
+      )}
+
+      {summary.editingOn ? (
+        <div>
+          <input placeholder={summary.fieldText} />
+          <button
+            onClick={() =>
+              setSummary({ fieldText: summary.fieldText, editingOn: false })
+            }
+          >
+            X
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p id="summary">{summary.fieldText}</p>
+          <button
+            onClick={() =>
+              setSummary({ fieldText: summary.fieldText, editingOn: true })
+            }
+          >
+            Edit
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
 function editField (props) {
