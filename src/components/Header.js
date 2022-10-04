b5c24b725ca02c652f0d35f40036ea79c7e36aff
mochadreamboat <jersotmac@gmail.com>
2022-10-03 19:27:41 -0500

    Overhauled header component by refactoring into functional component utilizing hooks.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,120 @@
 // personalInfo.title
 //personalInfo.pic
 
-import React, { Component } from "react";
-class Header extends Component {
-    constructor(props) {
-        super(props);
-    }
+import React, { useState } from "react";
 
-    render() {
-        const { name, title, summary, showForm } = this.props;
-
-        return (
-            <div>
-                <section>
-                    <h1>{name}</h1>
-                    <h2>{title}</h2>
-                    <p>{summary}</p>
-                    <button id="header" onClick={showForm}>Edit</button>
-                </section>
-            </div>
- );
+function Header () {
+    // Each field has a state object holding key/value pair for content and whether or not user is editing.
+  
+    const [name, setName] = useState({
+      fieldText: "insert name here",
+      editingOn: false,
+    });
+    const [title, setTitle] = useState({
+      fieldText: "insert title here",
+      editingOn: false,
+    });
+    const [summary, setSummary] = useState({
+      fieldText: "lorem ipsum something something...",
+      editingOn: false,
+    });
+  
+    const submitChange = (field, setField) => {
+      const change = document.getElementById(field).value;
+      setField({fieldText: change, editingOn: false});
     }
-}
+  
+    // Should include a button that appears when hovered
+    // That lets user edit field in place.
+    // Conditonal rendering based on whether or not editing is T or F.
+    return (
+      <section>
+        {name.editingOn ? (
+          <div>
+            <input placeholder={name.fieldText} id="name"/>
+            <button
+              onClick={() => submitChange("name", setName)}
+            >
+              Change
+            </button>
+            <button
+              onClick={() =>
+                setName({ fieldText: name.fieldText, editingOn: false })
+              }
+            >
+              X
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1 id="name">{name.fieldText}</h1>
+            <button
+              onClick={() =>
+                setName({ fieldText: name.fieldText, editingOn: true })
+              }
+            >
+              Edit
+            </button>
+          </div>
+        )}
+  
+        {title.editingOn ? (
+          <div>
+            <input placeholder={title.fieldText} id="title"/>
+            <button
+              onClick={() => submitChange("title", setTitle)}
+            >Change
+            </button>
+            <button
+              onClick={() =>
+                setTitle({ fieldText: title.fieldText, editingOn: false })
+              }
+            >
+              X
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h2 id="title">{title.fieldText}</h2>
+            <button
+              onClick={() =>
+                setTitle({ fieldText: title.fieldText, editingOn: true })
+              }
+            >
+              Edit
+            </button>
+          </div>
+        )}
+  
+        {summary.editingOn ? (
+          <div>
+            <input placeholder={summary.fieldText} id="summary"/>
+            <button
+              onClick={() => submitChange("summary", setSummary)}
+            >Change
+            </button>
+            <button
+              onClick={() =>
+                setSummary({ fieldText: summary.fieldText, editingOn: false })
+              }
+            >
+              X
+            </button>
+          </div>
+        ) : (
+          <div>
+            <p id="summary">{summary.fieldText}</p>
+            <button
+              onClick={() =>
+                setSummary({ fieldText: summary.fieldText, editingOn: true })
+              }
+            >
+              Edit
+            </button>
+          </div>
+        )}
+      </section>
+    );
+  }
 
 export default Header;
